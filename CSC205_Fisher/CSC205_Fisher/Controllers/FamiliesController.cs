2a50cadfa469503ba7751b306bf9fadbb9124ee1
FishOfMen <zack.fisher@geneva.edu>
2016-10-13 19:30:05 -0400

    Created all View folders and views. Also added all controllers.

---
+++ CSC205_Fisher/CSC205_Fisher/Controllers/FamiliesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSC205_Fisher.Controllers
+{
+    public class FamiliesController : Controller
+    {
+        // GET: Families
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
